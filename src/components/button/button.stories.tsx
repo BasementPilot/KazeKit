6fad31af2c3b51c24b8886d44f5edc58b9cf4043
BasementPilot <basementaeronaut@gmail.com>
2023-09-13 20:50:26 +0200

    updated package.json for storybook/react

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -39,12 +39,10 @@ export const Default: Story = {
 };
 
 export const Primary = {
-  name: 'Primary',
   render: () => <Button onClick$={onClick$} text="Primary" variant="primary" />,
 };
 
 export const ButtonWithIcon = {
-  name: 'Button With Icon',
   render: () => (
     <Button onClick$={onClick$} text="Primary" variant="primary">
       <div q:slot="icon" style={{ color: 'white', fontSize: '24px' }}>
@@ -55,21 +53,17 @@ export const ButtonWithIcon = {
 };
 
 export const Secondary = {
-  name: 'Secondary',
   render: () => <Button onClick$={onClick$} text="Secondary" variant="secondary" />,
 };
 
 export const Success = {
-  name: 'Success',
   render: () => <Button onClick$={onClick$} text="Success" variant="success" />,
 };
 
 export const Danger = {
-  name: 'Danger',
   render: () => <Button onClick$={onClick$} text="Danger" variant="danger" />,
 };
 
 export const Warning = {
-  name: 'Warning',
   render: () => <Button onClick$={onClick$} text="Warning" variant="warning" />,
 };
